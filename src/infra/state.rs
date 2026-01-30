08fabf8e69e024de775b68b6b5476dcc1670675c
Alexander (wolf) <qrer71@yandex.ru>
2026-01-30 23:30:54 +0300

    Add factory method for user creation interactor

--- src/infra/state.rs
+++ src/infra/state.rs
@@ -1,4 +1,8 @@
+use crate::adapter::db::session::SqlxSession;
+use crate::application::app_error::AppResult;
+use crate::application::interactors::users::CreateUserInteractor;
 use sqlx::{Pool, Postgres};
+use std::sync::Arc;
 
 #[derive(Clone)]
 pub struct AppState {
@@ -6,5 +10,9 @@ pub struct AppState {
 }
 
 impl AppState {
+    pub async fn create_user_interactor(&self) -> AppResult<CreateUserInteractor> {
+        let session = SqlxSession::new(self.pool.clone()).await?;
 
+        Ok(CreateUserInteractor::new(Arc::new(session)))
+    }
 }
