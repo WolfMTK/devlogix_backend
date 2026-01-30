51be23a92919ddae74003c9dca5fadb4e5cf8cf4
Alexander (wolf) <qrer71@yandex.ru>
2026-01-30 21:58:21 +0300

    Add database transaction trait

---
+++ src/application/interface/db.rs
@@ -0,0 +1,8 @@
+use crate::application::app_error::AppResult;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait DBSession: Send + Sync {
+    async fn commit(&self) -> AppResult<()>;
+    async fn rollback(&self) -> AppResult<()>;
+}
