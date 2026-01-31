c0d23ad8f4a6d79aaf2b6d07fd5a7bcf88dab3cb
Alexander (wolf) <qrer71@yandex.ru>
2026-01-31 22:15:40 +0300

    Add UserReader trait for user query operations

--- src/application/interface/gateway/user.rs
+++ src/application/interface/gateway/user.rs
@@ -1,13 +1,16 @@
 use crate::{
     application::app_error::AppResult,
-    domain::entities::{
-        id::Id,
-        user::User
-    }
+    domain::entities::{id::Id, user::User},
 };
 use async_trait::async_trait;
 
 #[async_trait]
 pub trait UserWriter: Send + Sync {
     async fn insert(&self, user: User) -> AppResult<Id<User>>;
+}
+
+#[async_trait]
+pub trait UserReader: Send + Sync {
+    async fn find_by_email(&self, email: &str) -> AppResult<Option<User>>;
+    async fn find_by_id(&self, user_id: &Id<User>) -> AppResult<Option<User>>;
 }
