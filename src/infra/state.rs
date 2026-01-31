d50e6cc7136db51762caf74efb7ed524200612f1
Alexander (wolf) <qrer71@yandex.ru>
2026-01-31 16:00:45 +0300

    Add axum extractor for CreateUserInteractor

--- src/infra/state.rs
+++ src/infra/state.rs
@@ -1,6 +1,18 @@
-use crate::adapter::db::session::SqlxSession;
-use crate::application::app_error::AppResult;
-use crate::application::interactors::users::CreateUserInteractor;
+use crate::{
+    adapter::db::{
+        gateway::user::UserGateway,
+        session::SqlxSession
+    },
+    application::{
+        app_error::{AppError, AppResult},
+        interactors::users::CreateUserInteractor
+    }
+};
+use async_trait::async_trait;
+use axum::{
+    extract::{FromRef, FromRequestParts},
+    http::request::Parts
+};
 use sqlx::{Pool, Postgres};
 use std::sync::Arc;
 
@@ -9,10 +21,33 @@ pub struct AppState {
     pub pool: Pool<Postgres>,
 }
 
-impl AppState {
-    pub async fn create_user_interactor(&self) -> AppResult<CreateUserInteractor> {
-        let session = SqlxSession::new(self.pool.clone()).await?;
+#[async_trait]
+pub trait FromAppState: Sized {
+    async fn from_app_state(state: &AppState) -> AppResult<Self>;
+}
 
-        Ok(CreateUserInteractor::new(Arc::new(session)))
+#[async_trait]
+impl FromAppState for CreateUserInteractor {
+    async fn from_app_state(state: &AppState) -> AppResult<Self> {
+        let session = SqlxSession::new(state.pool.clone()).await?;
+        let user_gateway = UserGateway::new(session.clone());
+
+        Ok(CreateUserInteractor::new(
+            Arc::new(session),
+            Arc::new(user_gateway),
+        ))
+    }
+}
+
+impl<S> FromRequestParts<S> for CreateUserInteractor
+where
+    S: Send + Sync,
+    AppState: FromRef<S>,
+{
+    type Rejection = AppError;
+
+    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let app_state = AppState::from_ref(state);
+        CreateUserInteractor::from_app_state(&app_state).await
     }
 }
