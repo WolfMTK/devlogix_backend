e3fa079697f478a94b8aed8462ffcf18803099da
Alexander (wolf) <qrer71@yandex.ru>
2026-01-31 16:06:52 +0300

    Add session and ID validation errors

--- src/application/app_error.rs
+++ src/application/app_error.rs
@@ -1,9 +1,19 @@
 use thiserror::Error;
 
+// TODO: Add new exceptions and a description
 #[derive(Error, Debug)]
 pub enum AppError {
     #[error("Database error: {0}")]
     DatabaseError(#[from] sqlx::Error),
+
+    #[error("Invalid Id: {0}")]
+    InvalidId(String),
+
+    #[error("")]
+    SessionAlreadyCommitted,
+
+    #[error("")]
+    SessionAlreadyRolledBack,
 }
 
 pub type AppResult<T> = Result<T, AppError>;
