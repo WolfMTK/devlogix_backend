fa3799c486f4bb25dc0ee16e2a03d2415aaee4c3
Alexander (wolf) <qrer71@yandex.ru>
2026-02-01 17:11:44 +0300

    Add UserDTO for user data representation

--- src/application/dto/user.rs
+++ src/application/dto/user.rs
@@ -1,6 +1,17 @@
+use chrono::{DateTime, Utc};
+
 #[derive(Debug)]
 pub struct CreateUserDTO {
     pub username: String,
     pub email: String,
     pub password: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct UserDTO {
+    pub id: String,
+    pub username: String,
+    pub email: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
 }
