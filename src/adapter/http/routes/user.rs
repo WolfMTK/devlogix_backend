f2f92426cefebc77233657da60fc579efbe02a05
Alexander (wolf) <qrer71@yandex.ru>
2026-02-03 11:36:05 +0300

    Add get_me endpoint with authentication

--- src/adapter/http/routes/user.rs
+++ src/adapter/http/routes/user.rs
@@ -1,19 +1,27 @@
 use crate::{
     adapter::http::{
-        schema::id::IdResponse,
-        schema::user::CreateUserRequest
+        middleware::extractor::AuthUser,
+        schema::{
+            id::IdResponse,
+            user::{
+                CreateUserRequest,
+                GetUserResponse
+            },
+        }
     },
     application::{
         app_error::AppResult,
-        dto::user::CreateUserDTO,
-        interactors::users::CreateUserInteractor
+        dto::{
+            id::IdDTO,
+            user::CreateUserDTO
+        },
+        interactors::users::{
+            CreateUserInteractor,
+            GetMeInteractor
+        },
     }
 };
-use axum::{
-    http::StatusCode,
-    response::IntoResponse,
-    Json
-};
+use axum::{http::StatusCode, response::IntoResponse, Json};
 
 pub async fn register(
     interactor: CreateUserInteractor,
@@ -25,8 +33,24 @@ pub async fn register(
         password: payload.password,
     };
     let user_id = interactor.execute(dto).await?;
-    let response = IdResponse {
-      id: user_id.id,
+    let response = IdResponse { id: user_id.id };
+    Ok((StatusCode::OK, Json(response)))
+}
+
+pub async fn get_me(
+    auth_user: AuthUser,
+    interactor: GetMeInteractor,
+) -> AppResult<impl IntoResponse> {
+    let dto = IdDTO {
+        id: auth_user.user_id,
+    };
+    let user = interactor.execute(dto).await?;
+    let response = GetUserResponse {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
     };
     Ok((StatusCode::OK, Json(response)))
 }
