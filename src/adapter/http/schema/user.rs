80e9bb65e35badb1450f2238ea4771c2e1857170
Alexander (wolf) <qrer71@yandex.ru>
2026-02-03 11:35:33 +0300

    Add GetUserResponse struct and improve imports

--- src/adapter/http/schema/user.rs
+++ src/adapter/http/schema/user.rs
@@ -1,8 +1,18 @@
-use serde::Deserialize;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
 pub struct CreateUserRequest {
     pub username: String,
     pub email: String,
-    pub password: String
+    pub password: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct GetUserResponse {
+    pub id: String,
+    pub username: String,
+    pub email: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
 }
