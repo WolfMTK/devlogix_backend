e49fe8bfbd49b3b3cbfa253edc5bb1b0fda432ca
Alexander (wolf) <qrer71@yandex.ru>
2026-02-05 16:11:36 +0300

    Add unit tests for generic Id type

--- src/domain/entities/id.rs
+++ src/domain/entities/id.rs
@@ -31,3 +31,48 @@ impl <T> TryFrom<String> for Id<T> {
         Ok(Id::new(uuid))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::entities::id::Id;
+    use uuid::Uuid;
+
+    #[derive(Clone)]
+struct TestEntity;
+
+    #[test]
+    fn test_id_new() {
+        let uuid = Uuid::now_v7();
+        let id: Id<TestEntity> = Id::new(uuid);
+        assert_eq!(id.value, uuid)
+    }
+
+    #[test]
+    fn test_id_generate() {
+        let id1: Id<TestEntity> = Id::generate();
+        let id2: Id<TestEntity> = Id::generate();
+        assert_ne!(id1.value, id2.value);
+    }
+
+    #[test]
+    fn test_id_try_from_valid_uuid() {
+        let uuid = Uuid::now_v7();
+        let uuid_str = uuid.to_string();
+        let id: Id<TestEntity> = uuid_str.try_into().unwrap();
+        assert_eq!(id.value, uuid);
+    }
+
+    #[test]
+    fn test_id_try_from_invalid_uuid() {
+        let invalid_uuid = "invalid".to_owned();
+        let result: Result<Id<TestEntity>, _> = invalid_uuid.try_into();
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_id_clone() {
+        let id: Id<TestEntity> = Id::generate();
+        let cloned_id = id.clone();
+        assert_eq!(id.value, cloned_id.value);
+    }
+}
