34087abffbd011bfd54ddc0635ddd1f763179284
Alexander (wolf) <qrer71@yandex.ru>
2026-02-24 21:08:45 +0300

    Add methods get_project, get_all, count_projects, get

--- src/application/interface/gateway/project.rs
+++ src/application/interface/gateway/project.rs
@@ -3,6 +3,7 @@ use async_trait::async_trait;
 use crate::application::app_error::AppResult;
 use crate::domain::entities::id::Id;
 use crate::domain::entities::project::Project;
+use crate::domain::entities::workspace::Workspace;
 
 #[async_trait]
 pub trait ProjectWriter: Send + Sync {
@@ -12,4 +13,7 @@ pub trait ProjectWriter: Send + Sync {
 #[async_trait]
 pub trait ProjectReader: Send + Sync {
     async fn check_project_key(&self, project_key: &str) -> AppResult<bool>;
+    async fn get_all(&self, workspace_id: &Id<Workspace>, limit: i64, offset: i64) -> AppResult<Vec<Project>>;
+    async fn count_projects(&self, workspace_id: &Id<Workspace>) -> AppResult<i64>;
+    async fn get(&self, workspace_id: &Id<Workspace>, project_id: &Id<Project>) -> AppResult<Option<Project>>;
 }
