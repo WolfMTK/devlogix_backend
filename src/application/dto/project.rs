7b65b12c528abb9b63105e52507743dbf968adfc
Alexander (wolf) <qrer71@yandex.ru>
2026-02-24 21:08:37 +0300

    Add structures GetProjectListDTO, ProjectDTO, ProjectListDTO, GetProjectDTO

--- src/application/dto/project.rs
+++ src/application/dto/project.rs
@@ -1,3 +1,5 @@
+use chrono::{DateTime, Utc};
+
 #[derive(Debug, Clone)]
 pub struct CreateProjectDTO {
     pub user_id: String,
@@ -7,4 +9,40 @@ pub struct CreateProjectDTO {
     pub project_key: String,
     pub type_project: String,
     pub visibility: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct GetProjectListDTO {
+    pub user_id: String,
+    pub workspace_id: String,
+    pub page: i64,
+    pub per_page: i64,
+}
+
+#[derive(Debug, Clone)]
+pub struct ProjectDTO {
+    pub id: String,
+    pub workspace_id: String,
+    pub name: String,
+    pub description: Option<String>,
+    pub project_key: String,
+    pub type_project: String,
+    pub visibility: String,
+    pub updated_at: DateTime<Utc>,
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Clone)]
+pub struct ProjectListDTO {
+    pub total: i64,
+    pub page: i64,
+    pub per_page: i64,
+    pub items: Vec<ProjectDTO>,
+}
+
+#[derive(Debug, Clone)]
+pub struct GetProjectDTO {
+    pub user_id: String,
+    pub workspace_id: String,
+    pub project_id: String,
 }
